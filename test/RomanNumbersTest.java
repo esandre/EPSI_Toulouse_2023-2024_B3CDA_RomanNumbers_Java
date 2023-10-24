25ff39cb61324d20584d8b9d6593cfc0254367f8
Enzo Sandré <atelier@enzosandre.fr>
2023-10-24 14:42:08 +0200

    Projet clean

---
+++ test/RomanNumbersTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RomanNumbersTest {
+
+    @Test
+    void testVide() {
+        assertTrue(true);
+    }
+}
