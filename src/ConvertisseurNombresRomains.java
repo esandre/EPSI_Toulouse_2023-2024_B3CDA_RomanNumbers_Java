bb6d7d62c0376d0a2b263c288c380ed86ea398a5
Enzo Sandré <atelier@enzosandre.fr>
2023-10-25 10:05:12 +0200

    4 -> IV

--- src/ConvertisseurNombresRomains.java
+++ src/ConvertisseurNombresRomains.java
@@ -2,6 +2,7 @@ public class ConvertisseurNombresRomains {
     public static String Convertir(int chiffreArabe) {
         if (chiffreArabe == 1) return "I";
         if (chiffreArabe == 2) return "II";
-        return "III";
+        if (chiffreArabe == 3) return "III";
+        return "IIII";
     }
 }
